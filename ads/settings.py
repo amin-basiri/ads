1bfa532cee12832f7615c396ea899498cfa91328
Amin <amin.bsr99@gmail.com>
2023-10-02 15:12:06 -0700

    DB Postgresql

--- ads/settings.py
+++ ads/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'ads.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "ads",
+        "USER": "ads_user",
+        "PASSWORD": "ads@123",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
